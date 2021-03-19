25f86da5742e1bcdf0b6d912da14a5ee4bf4e3ed
Brian Lin <blin@cs.wisc.edu>
2021-03-19 16:39:47 -0500

    Debug nuclear option
    D_SECURITY:2 didn't do it for whatever reason?
    
    2021-03-19T21:02:57.1432004Z 03/19/21 21:01:15 (D_ALWAYS:2) condor_read(): Socket closed when trying to read 5 bytes from <172.17.0.2:4453> in non-blocking mode
    2021-03-19T21:02:57.1432883Z 03/19/21 21:01:15 (D_ALWAYS:2) IO: EOF reading packet header
    2021-03-19T21:02:57.1433555Z 03/19/21 21:01:15 (D_ALWAYS:2) condor_read(): Socket closed when trying to read 5 bytes from <172.17.0.2:4453>
    2021-03-19T21:02:57.1434231Z 03/19/21 21:01:15 (D_ALWAYS:2) IO: EOF reading packet header
    2021-03-19T21:02:57.1435229Z 03/19/21 21:01:15 (D_ALWAYS:2) condor_read(): Socket closed when trying to read 5 bytes from <172.17.0.2:4453> in non-blocking mode
    2021-03-19T21:02:57.1436080Z 03/19/21 21:01:15 (D_ALWAYS:2) IO: EOF reading packet header
    2021-03-19T21:02:57.1436720Z 03/19/21 21:01:15 (D_ALWAYS:2) condor_read(): Socket closed when trying to read 5 bytes from <172.17.0.2:4453>
    2021-03-19T21:02:57.1437382Z 03/19/21 21:01:15 (D_ALWAYS:2) IO: EOF reading packet header
    2021-03-19T21:02:57.1437960Z 03/19/21 21:01:15 (D_ALWAYS) AUTHENTICATE: handshake failed!
    2021-03-19T21:02:57.1439061Z 03/19/21 21:01:15 (D_ALWAYS) DC_AUTHENTICATE: required authentication of 172.17.0.2 failed: AUTHENTICATE:1002:Failure performing handshake|AUTHENTICATE:1004:Failed to authenticate using SCITOKENS

--- tests/test_inside_docker.sh
+++ tests/test_inside_docker.sh
@@ -84,7 +84,7 @@ make install PYTHON=/usr/bin/python3
 popd
 
 cat <<EOF > /etc/condor-ce/config.d/99-local.conf
-SCHEDD_DEBUG = $(SCHEDD_DEBUG) D_CAT D_ALWAYS:2 D_SECURITY:2
+SCHEDD_DEBUG = $(SCHEDD_DEBUG) D_CAT D_ALWAYS:2 D_FULLDEBUG D_SECURITY:2
 EOF
 
 # Bind on the right interface and skip hostname checks.
